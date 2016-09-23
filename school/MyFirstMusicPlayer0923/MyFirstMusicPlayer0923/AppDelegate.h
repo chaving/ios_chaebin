498e20148207cd8a830c63ed5417d68c4ea42b6d
chaving <lcb0811@gmail.com>
2016-09-23 23:20:55 +0900

    Add 0923 Status

---
+++ school/MyFirstMusicPlayer0923/MyFirstMusicPlayer0923/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstMusicPlayer0923
+//
+//  Created by chaving on 2016. 9. 23..
+//  Copyright © 2016년 chaving. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
