ff04024b48202f10f3674bcaa64b615837c740e1
chaving <lcb0811@gmail.com>
2016-09-29 08:59:46 +0900

    Add 0928 Status

---
+++ school/Class0928/Class0928/Node.m
@@ -0,0 +1,13 @@
+//
+//  Node.m
+//  Class0928
+//
+//  Created by chaving on 2016. 9. 28..
+//  Copyright © 2016년 chaving. All rights reserved.
+//
+
+#import "Node.h"
+
+@implementation Node
+
+@end
---
+++ school/Class0928/Class0928/main.m
@@ -0,0 +1,63 @@
+//
+//  main.m
+//  Class0928
+//
+//  Created by chaving on 2016. 9. 28..
+//  Copyright © 2016년 chaving. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Node.h"
+#import "DoubleLinkedList.h"
+
+
+int main(int argc, const char * argv[]) {
+    
+    DoubleLinkedList *list01 = [[DoubleLinkedList alloc] init];
+    
+    [list01 addLastValue:1];
+    [list01 addLastValue:2];
+    [list01 addLastValue:3];
+    [list01 addLastValue:4];
+    [list01 addLastValue:5];
+    [list01 addFirstValue:6];
+    [list01 addLastValue:7];
+    
+//    [list01 addLastIndexNode:5 newValue:3];
+    
+    [list01 printAllNode];
+    NSLog(@"-------------------------------------");
+    [list01 removeLast];
+    
+    [list01 printAllNode];
+    NSLog(@"-------------------------------------");
+    
+    [list01 removeFirst];
+    
+    [list01 printAllNode];
+    
+    NSLog(@"-------------------------------------");
+    
+    [list01 removeFirst];
+    
+    [list01 printAllNode];
+    
+    NSLog(@"-------------------------------------");
+    
+    [list01 countAll];
+    
+    NSLog(@"-------------------------------------");
+    
+    [list01 addFirstValue:6];
+    
+    [list01 printAllNode];
+    
+    NSLog(@"-------------------------------------");
+    
+    [list01 countAll];
+    
+    [list01 countAll];
+    
+    
+    return 0;
+}
