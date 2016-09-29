c18b4798ff708aa3b75532ebf25c05c069e421b5
chaving <lcb0811@gmail.com>
2016-09-29 19:57:19 +0900

    Add 0929 Status

---
+++ school/class0929_UITest/class0929_UITest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  class0929_UITest
+//
+//  Created by chaving on 2016. 9. 29..
+//  Copyright © 2016년 chaving. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
