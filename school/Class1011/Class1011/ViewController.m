956665e74e4ff0dc7ec6388bfc979c2a38911a8f
chaving <lcb0811@gmail.com>
2017-01-02 17:30:26 +0900

    Now State

--- school/Class1011/Class1011/ViewController.m
+++ school/Class1011/Class1011/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 
-@interface ViewController () <UIScrollViewDelegate>
+@interface ViewController () <UIScrollViewDelegate, UITextFieldDelegate>
 
 @property UILabel *displayLayer;
 @property UITextField *textFildeLayer;
